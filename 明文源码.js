e9551caf5fe6da84fa9218d1560f8677a59500c1
Dirinkbottle <122192254+Dirinkbottle@users.noreply.github.com>
2025-01-31 20:15:11 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '769fda72-7209-40e2-bba3-bf5888cf5f3b';
 let proxyIP = '';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLmZ4eGsuZGVkeW4uaW8=');
